3196ea290de458c2eae7b887ca1e24ff8aea28be
yeongtze <yeongtze@googlemail.com>
2019-04-03 17:30:22 +0800

    Added suppoort for SSM send-command and get-command-invocation. Added test project.

--- AwsSsmHandler.cs
+++ AwsSsmHandler.cs
@@ -1,24 +1,84 @@
-﻿using Synapse.Core;
+﻿using Newtonsoft.Json;
+using Synapse.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
-
+using Amazon;
+using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
 
 public class AwsSsmHandler : HandlerRuntimeBase
 {
-    private readonly ExecuteResult _result = new ExecuteResult()
-    {
-        Status = StatusType.None,
-        BranchStatus = StatusType.None,
-        Sequence = int.MaxValue
-    };
+    private HandlerConfig _config;
+    private string _ProgressMessage = "";
 
     public override ExecuteResult Execute(HandlerStartInfo startInfo)
     {
-        string exitData = "This is a test";
-        _result.ExitData = exitData;
+        int sequenceNumber = 0;
+        string context = "Execute";
+        SsmCommandResponse response = null;
+        UserRequest request = null;
+        ExecuteResult _result = new ExecuteResult()
+        {
+            Status = StatusType.None,
+            BranchStatus = StatusType.None,
+            Sequence = int.MaxValue
+        };
+
+        try
+        {
+            _ProgressMessage = "Parsing incoming request...";
+            _result.Status = StatusType.Initializing;
+            ++sequenceNumber;
+            OnProgress(context, _ProgressMessage, _result.Status, sequenceNumber);
+            OnLogMessage(context, _ProgressMessage);
+            request = DeserializeOrNew<UserRequest>(startInfo.Parameters);
+
+            _ProgressMessage = "Executing request" + (startInfo.IsDryRun ? " in dry run mode..." : "...");
+            _result.Status = StatusType.Running;
+            ++sequenceNumber;
+            OnProgress(context, _ProgressMessage, _result.Status, sequenceNumber);
+            OnLogMessage(context, _ProgressMessage);
+
+            if (ValidateRequest(request))
+            {
+                if (!startInfo.IsDryRun)
+                {
+                    response = ExecuteSsmCommand(request, _config).Result;
+                    _ProgressMessage = "Execution is completed.";
+                    response.Summary = _ProgressMessage;
+                }
+                else
+                {
+                    _ProgressMessage = "Dry run execution is completed.";
+                }
+                _result.Status = StatusType.Complete;
+                OnLogMessage(context, _ProgressMessage);
+            }
+        }
+        catch (Exception ex)
+        {
+            string errorMessage = ex.InnerException != null ? ex.InnerException.Message : ex.Message;
+            _result.Status = StatusType.Failed;
+            _ProgressMessage = $"Execution has been aborted due to: {errorMessage}";
+            response = new SsmCommandResponse()
+            {
+                Success = false,
+                Summary = _ProgressMessage,
+                OriginalRequest = request
+            };
+            OnLogMessage(context, _ProgressMessage, LogLevel.Error);
+        }
+
+        _result.ExitData = JsonConvert.SerializeObject(response);
+        _result.Sequence = int.MaxValue;
+        OnProgress(context, _ProgressMessage, _result.Status, int.MaxValue);
+
         return _result;
     }
 
@@ -35,6 +95,20 @@ public class AwsSsmHandler : HandlerRuntimeBase
         };
     }
 
+    public override IHandlerRuntime Initialize(string values)
+    {
+        try
+        {
+            _config = DeserializeOrNew<HandlerConfig>(values) ?? new HandlerConfig();
+        }
+        catch (Exception ex)
+        {
+            OnLogMessage("Initialization", "Encountered exception while deserializing handler config.", LogLevel.Error, ex);
+        }
+
+        return this;
+    }
+
     public override object GetParametersInstance()
     {
         List<string> script = new List<string> { "$env:computername" };
@@ -44,26 +118,243 @@ public class AwsSsmHandler : HandlerRuntimeBase
         return new UserRequest
         {
             InstanceId = "i-12345678",
-            InstanceName = "aaaaaaaa",
             CommandType = "send-command", // "get-command-invocation"
-            UserId = "bbbbbbbb",
+            CommandId = "xxxxxxxx",
             CommandDocument = "AWS-RunPowerShellScript",
             CommandParameters = commandParameters,
-            CommandComment = "cccccccc",
-            Region = "eu-west-1"
+            CommandComment = "xxxxxxxx",
+            AwsRegion = "eu-west-1",
+            AwsRole = "xxxxxxxx"
         };
+    }
+
+    public static bool ValidateRequest(UserRequest request)
+    {
+        string errorMessage = string.Empty;
+
+        if (request == null)
+        {
+            errorMessage = "Request cannot be null or empty.";
+        }
+        else if (string.IsNullOrWhiteSpace(request.InstanceId))
+        {
+            errorMessage = "Instance id cannot be null or empty.";
+        }
+        else if (!IsValidCommandType(request.CommandType))
+        {
+            errorMessage = "Command type cannot be null or empty.";
+        }
+        else if (request.CommandType.ToLowerInvariant() == "get-command-invocation" && string.IsNullOrWhiteSpace(request.CommandId))
+        {
+            errorMessage = "Command id cannot be null or empty for 'get-command-invocation'.";
+        }
+        else if (request.CommandType.ToLowerInvariant() == "send-command" && string.IsNullOrWhiteSpace(request.CommandDocument))
+        {
+            errorMessage = "Command document cannot be null or empty for 'send-command'.";
+        }
+        else if (!IsAwsRegion(request.AwsRegion))
+        {
+            errorMessage = "AWS region specified is not valid.";
+        }
+        else if (string.IsNullOrWhiteSpace(request.AwsRole))
+        {
+            errorMessage = "AWS role cannot be null or empty.";
+        }
+
+        if (!string.IsNullOrWhiteSpace(errorMessage))
+        {
+            throw new Exception(errorMessage);
+        }
+        return true;
+    }
+
+    public static bool IsValidCommandType(string commandType)
+    {
+        bool isValid = false;
+
+        if (!string.IsNullOrWhiteSpace(commandType))
+        {
+            commandType = commandType.ToLowerInvariant();
+            switch (commandType)
+            {
+                case "get-command-invocation":
+                    isValid = true;
+                    break;
+                case "send-command":
+                    isValid = true;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        return isValid;
+    }
+
+    public static bool IsAwsRegion(string region)
+    {
+        if (region == null) return false;
+
+        return RegionEndpoint.GetBySystemName(region).DisplayName != "Unknown";
+    }
+
+    public async Task<SsmCommandResponse> ExecuteSsmCommand(UserRequest request, HandlerConfig config)
+    {
+        if (request == null || config == null) return null;
+
+        bool isSuccess = false;
+        string errorMessage = string.Empty;
+        SsmCommandResponse output = null;
+
+        AmazonSimpleSystemsManagementConfig clientConfig = new AmazonSimpleSystemsManagementConfig()
+        {
+            MaxErrorRetry = config.ClientMaxErrorRetry,
+            Timeout = TimeSpan.FromSeconds(config.ClientTimeoutSeconds),
+            ReadWriteTimeout = TimeSpan.FromSeconds(config.ClientReadWriteTimeoutSeconds),
+            RegionEndpoint = RegionEndpoint.GetBySystemName(request.AwsRegion) // Or RegionEndpoint.EUWest1
+        };
+
+        try
+        {
+            var chain = new CredentialProfileStoreChain();
+            AWSCredentials awsCredentials;
+            if (chain.TryGetAWSCredentials(request.AwsRole, out awsCredentials))
+            {
+                // use awsCredentials
+                AmazonSimpleSystemsManagementClient ssmClient =
+                    new AmazonSimpleSystemsManagementClient(awsCredentials, clientConfig);
+                if (request.CommandType == "send-command")
+                {
+                    List<string> instanceIds = new List<string> {request.InstanceId};
+                    SendCommandRequest commandRequest = new SendCommandRequest(request.CommandDocument, instanceIds);
+                    commandRequest.MaxConcurrency = config.CommandMaxConcurrency; // 50%
+                    commandRequest.MaxErrors = config.CommandMaxErrors;
+                    commandRequest.TimeoutSeconds = config.CommandTimeoutSeconds;
+                    commandRequest.Comment = request.CommandComment;
+                    commandRequest.Parameters = request.CommandParameters;
+                    SendCommandResponse sendCommandResponse = await ssmClient.SendCommandAsync(commandRequest);
+
+                    isSuccess = true;
+
+                    output = new SsmCommandResponse
+                    {
+                        Success = isSuccess,
+                        CommandId = sendCommandResponse.Command.CommandId,
+                        CommandStatus = sendCommandResponse.Command.StatusDetails,
+                        ErrorMessage = errorMessage,
+                        CommandComment = sendCommandResponse.Command.Comment,
+                        OriginalRequest = request
+                    };
+                }
+                else if (request.CommandType == "get-command-invocation")
+                {
+                    GetCommandInvocationRequest commandRequest = new GetCommandInvocationRequest()
+                    {
+                        CommandId = request.CommandId,
+                        InstanceId = request.InstanceId
+                    };
+
+                    GetCommandInvocationResponse getCommandResponse =
+                        await ssmClient.GetCommandInvocationAsync(commandRequest);
+
+                    output = new SsmCommandResponse
+                    {
+                        Success = isSuccess,
+                        CommandId = getCommandResponse.CommandId,
+                        CommandStatus = getCommandResponse.StatusDetails,
+                        ErrorMessage = errorMessage,
+                        StandardOutput = getCommandResponse.StandardOutputContent,
+                        StandardError = getCommandResponse.StandardErrorContent,
+                        CommandComment = getCommandResponse.Comment,
+                        OriginalRequest = request
+                    };
+                }
+            }
+            else
+            {
+                throw new Exception("AWS credentials cannot be found for the operation.");
+            }
+        }
+        catch (AmazonSimpleSystemsManagementException ex)
+        {
+            switch (ex.ErrorCode)
+            {
+                // https://docs.aws.amazon.com/systems-manager/latest/APIReference/API_SendCommand.html
+                // Error codes for "SendCommandRequest"
+                case "DuplicateInstanceId":
+                    errorMessage = "You cannot specify an instance ID in more than one association.";
+                    break;
+                case "InternalServerError":
+                    errorMessage = "Internal server error.";
+                    break;
+                case "InvalidDocument":
+                    errorMessage = "The specified document does not exist.";
+                    break;
+                case "InvalidDocumentVersion":
+                    errorMessage = "The document version is not valid or does not exist.";
+                    break;
+                case "ExpiredTokenException":
+                    errorMessage = "The security token included in the request is expired.";
+                    break;
+                case "InvalidInstanceId":
+                    errorMessage = "The instance is invalid.";
+                    break;
+                case "InvalidNotificationConfig":
+                    errorMessage = "One or more configuration items is not valid.";
+                    break;
+                case "InvalidOutputFolder":
+                    errorMessage = "The S3 bucket does not exist.";
+                    break;
+                case "InvalidParameters":
+                    errorMessage =
+                        "You must specify values for all required parameters in the Systems Manager document.";
+                    break;
+                case "InvalidRole":
+                    errorMessage = "The role name can't contain invalid characters.";
+                    break;
+                case "MaxDocumentSizeExceeded":
+                    errorMessage = "The size limit of a document is 64 KB.";
+                    break;
+                case "UnsupportedPlatformType":
+                    errorMessage = "The document does not support the platform type of the given instance ID(s).";
+                    break;
+                // Error codes for "GetcommandInvocation"
+                case "InvalidCommandId":
+                    errorMessage = "The command ID is invalid.";
+                    break;
+                case "InvocationDoesNotExist":
+                    errorMessage =
+                        "The command ID and instance ID you specified did not match any invocations. Verify the command ID adn the instance ID and try again.";
+                    break;
+                default:
+                    errorMessage = ex.Message;
+                    break;
+            }
+
+            throw new Exception(errorMessage);
+        }
+
+        return output;
     }
 
 }
 
 public class HandlerConfig
 {
-    public int ClientMaxErrorRetry { get; set; }
-    public int ClientTimeoutSeconds { get; set; }
-    public int ClientReadWriteTimeoutSeconds { get; set; }
-    public string CommandMaxConcurrency { get; set; }
-    public string CommandMaxErrors { get; set; }
-    public int CommandTimeoutSeconds { get; set; }
+    // https://docs.aws.amazon.com/sdk-for-net/v3/developer-guide/retries-timeouts.html
+    public int ClientMaxErrorRetry { get; set; } = 4;
+
+    public int ClientTimeoutSeconds { get; set; } = 100;
+
+    public int ClientReadWriteTimeoutSeconds { get; set; } = 300;
+
+    // https://docs.aws.amazon.com/systems-manager/latest/APIReference/API_SendCommand.html
+    public string CommandMaxConcurrency { get; set; } = "50";
+
+    public string CommandMaxErrors { get; set; } = "0";
+
+    public int CommandTimeoutSeconds { get; set; } = 3600;
 }
 
 public class SsmCommandResponse
@@ -82,6 +373,8 @@ public class SsmCommandResponse
 
     public string StandardError { get; set; }
 
+    public string Summary { get; set; }
+
     public UserRequest OriginalRequest { get; set; }
 }
 
@@ -89,13 +382,9 @@ public class UserRequest
 {
     public string InstanceId { get; set; }
 
-    public string InstanceName { get; set; }
-
-    public string CommandType { get; set; }
+    public string CommandType { get; set; } // "send-command", "get-command-invocation"
 
     public string CommandId { get; set; }
-
-    public string UserId { get; set; }
 
     public string CommandDocument { get; set; }
 
@@ -103,5 +392,7 @@ public class UserRequest
 
     public string CommandComment { get; set; }
 
-    public string Region { get; set; }
+    public string AwsRegion { get; set; }
+
+    public string AwsRole { get; set; }
 }
---
+++ handlers.SSM.net.Tests/AwsSsmHandlerTests.cs
@@ -0,0 +1,159 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handlers.SSM.net.Tests
+{
+    [TestFixture]
+    public class AwsSsmHandlerTests
+    {
+        [Test]
+        public void IsAwsRegion_Invalid_Aws_Region_Returns_False()
+        {
+            var result = AwsSsmHandler.IsAwsRegion("XXX");
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void IsAwsRegion_Valid_Aws_Region_Returns_True()
+        {
+            var result = AwsSsmHandler.IsAwsRegion("eu-west-1");
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void IsValidCommandType_Invalid_Command_Type_Returns_False()
+        {
+            var result = AwsSsmHandler.IsValidCommandType("XXX");
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void IsValidCommandType_Valid_Command_Type_Returns_True()
+        {
+            var result = AwsSsmHandler.IsValidCommandType("get-command-invocation");
+            Assert.IsTrue(result);
+            result = AwsSsmHandler.IsValidCommandType("send-command");
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void ValidateRequest_Empty_Request_Throws_Exception()
+        {
+            // Act
+            Exception ex = Assert.Throws<Exception>(() => AwsSsmHandler.ValidateRequest(null));
+
+            // Assert
+            Assert.That(ex.Message, Is.EqualTo("Request cannot be null or empty."));
+        }
+
+        [Test]
+        public void ValidateRequest_Empty_Instance_Id_Throws_Exception()
+        {
+            // Arrange
+            UserRequest request = new UserRequest();
+
+            // Act
+            Exception ex = Assert.Throws<Exception>(() => AwsSsmHandler.ValidateRequest(request));
+
+            // Assert
+            Assert.That(ex.Message, Is.EqualTo("Instance id cannot be null or empty."));
+        }
+
+        [Test]
+        public void ValidateRequest_Invalid_Command_Type_Throws_Exception()
+        {
+            // Arrange
+            UserRequest request = new UserRequest()
+            {
+                InstanceId = "i-xxxxxxxx"
+            };
+
+            // Act
+            Exception ex = Assert.Throws<Exception>(() => AwsSsmHandler.ValidateRequest(request));
+
+            // Assert
+            Assert.That(ex.Message, Is.EqualTo("Command type cannot be null or empty."));
+        }
+
+        [Test]
+        public void ValidateRequest_Empty_Command_Id_For_Get_Command_Invocation_Throws_Exception()
+        {
+            // Arrange
+            UserRequest request = new UserRequest()
+            {
+                InstanceId = "i-xxxxxxxx",
+                CommandType = "get-command-invocation"
+            };
+
+            // Act
+            Exception ex = Assert.Throws<Exception>(() => AwsSsmHandler.ValidateRequest(request));
+
+            // Assert
+            Assert.That(ex.Message, Is.EqualTo("Command id cannot be null or empty for 'get-command-invocation'."));
+        }
+
+        [Test]
+        public void ValidateRequest_Empty_Command_Document_For_Send_Command_Throws_Exception()
+        {
+            // Arrange
+            UserRequest request = new UserRequest()
+            {
+                InstanceId = "i-xxxxxxxx",
+                CommandType = "send-command"
+            };
+
+            // Act
+            Exception ex = Assert.Throws<Exception>(() => AwsSsmHandler.ValidateRequest(request));
+
+            // Assert
+            Assert.That(ex.Message, Is.EqualTo("Command document cannot be null or empty for 'send-command'."));
+        }
+
+        [Test]
+        public void ValidateRequest_Invalid_Aws_Region_Throws_Exception()
+        {
+            // Arrange
+            UserRequest request = new UserRequest()
+            {
+                InstanceId = "i-xxxxxxxx",
+                CommandType = "send-command",
+                CommandDocument = "xxxxxxxx"
+            };
+
+            // Act
+            Exception ex = Assert.Throws<Exception>(() => AwsSsmHandler.ValidateRequest(request));
+
+            // Assert
+            Assert.That(ex.Message, Is.EqualTo("AWS region specified is not valid."));
+        }
+
+        [Test]
+        public void ValidateRequest_Empty_Aws_Role_Throws_Exception()
+        {
+            // Arrange
+            UserRequest request = new UserRequest()
+            {
+                InstanceId = "i-xxxxxxxx",
+                CommandType = "send-command",
+                CommandDocument = "xxxxxxxx",
+                AwsRegion = "eu-west-1"
+            };
+
+            // Act
+            Exception ex = Assert.Throws<Exception>(() => AwsSsmHandler.ValidateRequest(request));
+
+            // Assert
+            Assert.That(ex.Message, Is.EqualTo("AWS role cannot be null or empty."));
+        }
+    }
+}
