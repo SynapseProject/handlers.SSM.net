f3edd78f67f22aea25191c89acd565bdc693cb41
yeongtze <yeongtze@googlemail.com>
2019-04-10 14:43:09 +0800

    Renamed solution, project and output assemblies.

--- Synapse.Handlers.SSM.Tests/Properties/AssemblyInfo.cs
+++ Synapse.Handlers.SSM.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("handlers.SSM.net.Tests")]
+[assembly: AssemblyTitle("Synapse.Handlers.SSM.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("handlers.SSM.net.Tests")]
+[assembly: AssemblyProduct("Synapse.Handlers.SSM.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
